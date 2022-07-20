bed2798c3e1627dfad1b5e2c7413d738fc32f12a
Mário Fernandes <mariobmf02@gmail.com>
2022-07-20 16:37:11 -0300

    Initial Commit

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,18 @@
+import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import React from 'react';
+
+export default function Home() {
+  const {user} = useUser();
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <pre>
+        {JSON.stringify(user, null, 2)}
+      </pre>
+    </div>
+  );
+
+}
+
+export const getServerSideProps = withPageAuthRequired();
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,29 @@
+import { getAccessToken, getSession } from "@auth0/nextjs-auth0";
+import { GetServerSideProps } from "next";
+
+export default function Home() {
+  return null;
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+  
+  if (!session) {    
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  } else {
+    const token = getAccessToken(req, res);
+    console.log(token);
+
+    return {
+      redirect: {
+        destination: "/app",
+        permanent: false,
+      },
+    };
+  }
+};
